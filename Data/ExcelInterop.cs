bbedea4716123208a4cb0d8868517a0212cc083f
rs <rs@INDIA-MSII58BG3>
2023-02-07 11:20:27 +0530

    Add project files.

---
+++ Data/ExcelInterop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BlazorGenerateExcelReport.Data
+{
+    public class ExcelInterop
+    {
+        public void CopySheet()
+        {
+            Excel.Application excelApp;
+
+            string sourceFileName = "";
+            string tempFileName = "";
+            string folderPath = @"";
+            string sourceFilePath = System.IO.Path.Combine(folderPath, sourceFileName);
+            string destinationFilePath = System.IO.Path.Combine(folderPath, tempFileName);
+
+            System.IO.File.Copy(sourceFileName, destinationFilePath, true);
+
+            excelApp = new Excel.Application();
+            Excel.Workbook wbSource, wbTarget;
+            Excel.Worksheet currentSheet;
+
+            wbSource = excelApp.Workbooks.Open(sourceFilePath);
+            wbTarget = excelApp.Workbooks.Open(destinationFilePath);
+
+            currentSheet = (Excel.Worksheet)wbSource.Worksheets["Sheet1"];
+            currentSheet.Name = "TempSheet";
+
+            currentSheet.Copy(wbTarget.Worksheets[1]);
+            wbSource.Close(false);
+            wbTarget.Close(true);
+            excelApp.Quit();
+
+            System.IO.File.Delete(sourceFilePath);
+            System.IO.File.Move(destinationFilePath, sourceFilePath);
+        }
+    }
+}
---
+++ Data/Student.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.JSInterop;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGenerateExcelReport.Data
+{
+    public class Student
+    {
+        public void GenerateExcel(IJSRuntime iJSRunTIme)
+        {
+            byte[] fileContents;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using(var package= new ExcelPackage())
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                workSheet.Cells[1, 1].Value = "Student Name";
+                workSheet.Cells[1, 1].Style.Font.Size = 12;
+                workSheet.Cells[1, 1].Style.Font.Bold = true;
+                workSheet.Cells[1, 1].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+
+                workSheet.Cells[1, 2].Value = "Student Roll";
+                workSheet.Cells[1, 2].Style.Font.Size = 12;
+                workSheet.Cells[1, 2].Style.Font.Bold = true;
+                workSheet.Cells[1, 2].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+
+
+                workSheet.Cells[2, 1].Value = "New Student 1";
+                workSheet.Cells[2, 1].Style.Font.Size = 12;
+                workSheet.Cells[2, 1].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+
+                workSheet.Cells[2, 2].Value = "1000";
+                workSheet.Cells[2, 2].Style.Font.Size = 12;
+                workSheet.Cells[2, 2].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+                workSheet.Cells[3, 1].Value = "New Student 2";
+                workSheet.Cells[3, 1].Style.Font.Size = 12;
+                workSheet.Cells[3, 1].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+
+                workSheet.Cells[3, 2].Value = "1001";
+                workSheet.Cells[3, 2].Style.Font.Size = 12;
+                workSheet.Cells[3, 2].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+                fileContents = package.GetAsByteArray();
+
+            }
+            iJSRunTIme.InvokeAsync<Student>(
+                "saveAsFile", "StudentList.xlsx",Convert.ToBase64String(fileContents)
+                );
+        }
+    }
+}
